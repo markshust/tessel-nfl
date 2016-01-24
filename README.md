410ee7a5053fc187d87e8007095d3708123e2f68
Mark Shust <mark@shust.com>
2016-01-24 16:29:09 -0500

    Updates to observe twitter stream for updates. This does not work on Tessel 1 due to faye-sockets implementation.

--- README.md
+++ README.md
@@ -1,46 +1,10 @@
 tessel-nfl
 ==========
 
-![Tessel NFL](http://markshust.com/sites/default/files/tessel-nfl.gif)
+Summary
+-------
 
-[View on YouTube](https://www.youtube.com/watch?v=CXc-9xZRk_M)
-
-Ever since I watched the World Cup at a BW3, and saw a [Budweiser Red Light](http://www.budweiser.ca/redlight/), I was fascinated with how it worked. When a team scored a goal on TV, the Red Light instantly lit up! Having worked with Firebase and websockets for a bit, I wanted to make that happen.
-
-I did some research and noticed that they used an [Electric Imp](http://electricimp.com/businesssolutions/casestudies/redlight/) and an app to control which game they wanted the Red Light to be activated on. So cool.
-
-After I pestered my wife to buy me a [tessel](https://tessel.io) for a while, I finally got one for my birthday! I instantly ordered the [relay module](http://start.tessel.io/modules/relay) and went to work. My favorite sport now is NFL football, and I thought how cool it would be to wire up the tessel to a light in my house for my annual Super Bowl party.
-
-I looked around for some streaming NFL score API's, but all of them were a few grand a month. Yah, no. I eventually stumped upon the NFL's basic XML feed and decided to use that. When I synced it up to the tessel, the XML to JSON module was taking roughly 5 seconds to process every call, which was unacceptable (this thing needs to be as real time as I can get! Otherwise what's the purpose...). I finally found a related JSON API, and even though it isn't formatted well and is super basic, it works just great once everything is all dialed in! I basically poll it every second and check the current score against the previously checked score, amongst other logic. And while it's not streaming/websockets, I can poll it every second without the NFL penalizing me and it accomplishes what I was looking for.
-
-Description
------------
-
-A script for the tessel that uses the [NFL's ScoreScript JSON feed](http://www.nfl.com/liveupdate/scorestrip/postseason/scorestrip.json) to check the scores for a specific game, and trigger an attached relay on a score.
-
-Prerequisites
--------------
-
-While you can port this script quite easily to a non-tessel node script, it's made to work with the [tessel](https://tessel.io) and it's [relay module](http://start.tessel.io/modules/relay). If you don't have the relay module, don't worry -- it'll still work, but it'll be quite more boring watching a command line when someone scores a game. Feel free to trigger something cooler than a light!
-
-Installation
-------------
-
-This is a great bit of starter code to start playing around with your tessel. Download the code, run `nom install`, then plugin your tessel and run `tessel push index.js`. You are now good to go! Easy peasy.
-
-Usage & Options
----------------
-
-This script is pretty much ready to go. I placed variables at the top of the file so that they are easy to modify depending on what you want to do.
-
-- `relayChannel`: Sets the relay channel to trigger.
-- `checkInterval`: How often to check the API for a score change, in milliseconds. A value of 1000 seems to work pretty well.
-- `checkLongInterval`: How often to check the API before the game starts or at halftime, in milliseconds. I think checking once a minute works well.
-- `toggleInterval`: How fast the relay pulses on a trigger, in milliseconds.
-- `toggleCount`: How often the relay should pulse. Make sure this is an even number so it goes back to the value before the relay is triggered.
-- `consoleActive`: Whether to output logs to the console on actions.
-- `url`: The URL to use of the API. This may be different if the NFL game is a [regular season game](http://www.nfl.com/liveupdate/scorestrip/scorestrip.json) vs. a [postseason game](http://www.nfl.com/liveupdate/scorestrip/postseason/scorestrip.json). 
-- `activeIndex`: Which index of the JSON response to activate for the game. I initially assigned this to 11 for the Super Bowl. The Pro Bowl this year should have the 10 index if you want to test this beforehand (I'm not sure if they will update the API as quick for the Pro Bowl, but for a real game it updates very quickly).
+This is a rewrite of [my original Firebase rendition](https://github.com/markoshust/tessel-nfl/tree/firebase), to instead look at the twitter handle [@NFL_LiveUpdate](https://twitter.com/NFL_LiveUpdate). The script to run that worker to populate Firebase is at [markoshust/firebase-nfl-live-update](https://github.com/markoshust/firebase-nfl-liveupdate), and the Twitter handle is a bot that listens on that Firebase, and it's source code is at [markoshust/twitter-nfl-liveupdate](https://github.com/markoshust/twitter-nfl-liveupdate).
 
 License
 -------
--- index.js
+++ index.js
@@ -1,156 +1,108 @@
-var http = require('http'),
-    tessel = require('tessel'),
-    relaylib = require('relay-mono'),
-    relay = relaylib.use(tessel.port['A']),
-    greenLed = tessel.led[0],
-    blueLed = tessel.led[1],
-    relayChannel = 1,
-    interval = 0,
-    checkInterval = 1000,
-    checkLongInterval = 60000,
-    toggleInterval = 400,
-    toggleCount = 10, // Make sure this is an even number so goes on/off properly
-    parserCount = 0,
-    visitorScore = 0,
-    homeScore = 0,
-    consoleActive = true,
-    checkOften = true,
-    gameOver = false,
-    trigger = false,
-    url = 'http://www.nfl.com/liveupdate/scorestrip/postseason/scorestrip.json',
-    activeIndex = 11,
-    parseResult = function(result) {
-        // Api result has been received and is being parsed
-        blueLed.output(0);
-
-        /*
-         *   0 => Day of week
-         *   1 => Time
-         *   2 => Quarter (1, 2, 3, 4, Halftime, Final, Pregame OR final overtime)
-         *   3 => UNKNOWN
-         *   4 => Visiting Team Full Name
-         *   5 => Visiting Team Abbreviation
-         *   6 => Visiting Team Score
-         *   7 => Home Team Full Name
-         *   8 => Home Team Abbreviation
-         *   9 => Home Team Score
-         *  10 => UNKNOWN
-         *  11 => UNKNOWN
-         *  12 => Game Identifier
-         *  13 => UNKNOWN
-         *  14 => TV Network
-         *  15 => Week Identifier
-         *  16 => Year
-         */
-        var resultVisitorScore = result[6],
-            resultHomeScore = result[9],
-            resultQuarter = result[2],
-            resultVisitorTeamName = result[4],
-            resultHomeTeamName = result[7];
-
-        if (resultQuarter == 'Pregame' || resultQuarter == 'Halftime') {
-            checkOften = false;
-            if (consoleActive) console.log('It\'s Miller Time!');
-        } else if (resultQuarter == 'Final' || resultQuarter == 'final overtime') {
-            blueLed.output(1);
-            greenLed.output(1);
-            gameOver = true;
-            checkOften = false;
-            if (consoleActive) {
-                console.log('Game is over. Final score is '
-                + resultVisitorTeamName + ' ' + resultVisitorScore + ', '
-                + resultHomeTeamName + ' ' + resultHomeScore + '.');
-            }
-        } else {
-            checkOften = true;
-            if (checkOften == false && consoleActive) console.log('Miller Time is over, let\'s get back to the game.');
-        }
-
-        if (homeScore != resultHomeScore) {
-            homeScore = resultHomeScore;
-            if (parserCount > 0) trigger = true;
-            if (consoleActive && parserCount != 0) console.log(resultHomeTeamName + ' have just scored!');
-        }
-
-        if (visitorScore != resultVisitorScore) {
-            visitorScore = resultVisitorScore;
-            if (parserCount > 0) trigger = true;
-            if (consoleActive && parserCount != 0) console.log(resultVisitorTeamName + ' have just scored!');
-        }
-
-        if (trigger && parserCount > 0) {
-            letsPartay();
-
-            trigger = false;
-
-            if (consoleActive) {
-                console.log('New score is '
-                    + resultVisitorTeamName + ' ' + resultVisitorScore + ', '
-                    + resultHomeTeamName + ' ' + resultHomeScore + '.');
-            }
-        } else if (checkOften) {
-            interval = setInterval(checkScore, checkInterval);
-        } else if (!gameOver) {
-            interval = setInterval(checkScore, checkLongInterval);
-        } else {
-            // Game is over, let's just keep the script open for 10 minutes (show the led's)
-            setTimeout(function() {}, 60000 * 10);
-        }
-
-        parserCount++;
-    },
-    checkScore = function() {
-        clearInterval(interval);
-
-        // Notify that the api call was sent
-        blueLed.output(1);
-
-        http.get(url, function(res) {
-            var json = '';
-
-            res.on('data', function(chunk) {
-                json += chunk;
-            });
-
-            res.on('end', function() {
-                // Clean up a sloppy api format
-                json = json.replace(/,,/g, ',"",').replace(/,,/g, ',"",');
-                // Let's get this into JSON
-                json = JSON.parse(json);
-                // Now we'll get the element we need
-                json = json.ss[activeIndex];
-
-                parseResult(json);
-            });
-        });
-    },
-    letsPartay = function() {
-        // Let's stop the checker, otherwise **)@#$%&*$# can happen
-        clearInterval(interval);
-
-        if (consoleActive) console.log('Let\'s partay!');
-
-        for (var i = 1; i <= toggleCount; i++) {
-            setTimeout(function() {
-                relay.toggle(relayChannel);
-                greenLed.toggle()
-            }, i * toggleInterval);
-        }
-
-        // Make sure we don't start back up again until the party is over
-        interval = setInterval(checkScore, (toggleCount + 1) * toggleInterval);
-    };
-
-relay.on('ready', function relayReady() {
-    // Let's make sure whatever we have plugged into the relay works
-    relay.turnOn(relayChannel);
-
-    setTimeout(function(){
-        relay.turnOff(relayChannel);
-
-        // Notify that the script started
-        blueLed.output(1);
-
-        interval = setInterval(checkScore, checkInterval);
-    }, 1000);
+var tessel = require('tessel');
+var relaylib = require('relay-mono');
+var Twitter = require('twitter');
+var twitterClient = new Twitter({
+  consumer_key: CONSUMER_KEY,
+  consumer_secret: CONSUMER_SECRET,
+  access_token_key: TOKEN_KEY,
+  access_token_secret: TOKEN_SECRET
 });
+var user = 'NFL_LiveUpdate';
+var hashtags = ['#NE', '#DEN', '#ARI', '#CAR'];
+var triggerWord = 'scored';
+var relay = relaylib.use(tessel.port['A']);
+var greenLed = tessel.led[0];
+var blueLed = tessel.led[1];
+var relayChannel = 1;
+var toggleCount = 10; // Make sure this is an even number so goes on/off properly
+var toggleInterval = 400;
+var isPartying = false;
+
+twitterClient.stream('user', {user: user}, function(stream) {
+  stream.on('data', function(tweet) {
+    console.log('Listening to tweet');
+
+    if (! tweet.text) return;
+
+    var isHashtagMatch = getIsHashtagMatch(tweet);
+    if (! isHashtagMatch) return;
+
+    var isScore = getIsScore(tweet);
+    if (! isScore) return;
+
+    letsPartay();
+  });
+
+  stream.on('error', function(error) {
+    console.log(error);
+  });
+});
+
+var getIsHashtagMatch = function(tweet) {
+  var isMatch = false;
+  var words = tweet.text.split(' ');
+
+  for (var i = 0; i < words.length; i++) {
+    if (hashtags.indexOf(words[i]) != -1) isMatch = true;
+  }
+
+  return isMatch;
+};
+
+var getIsScore = function(tweet) {
+  return tweet.text.indexOf(triggerWord) != -1
+};
+
+var letsPartay = function() {
+  // Don't run again until party is over otherwise **)@#$%&*$# can happen
+  if (isPartying) return;
+
+  isPartying = true;
+  console.log('Someone scored, let\'s partay!');
+
+  for (var i = 1; i <= toggleCount; i++) {
+    setTimeout(function() {
+      relay.toggle(relayChannel);
+      greenLed.toggle()
+    }, i * toggleInterval);
+  }
+
+  setTimeout(function() {
+    isPartying = false;
+  }, 10000);
+};
+
+/**
+ARI: Arizona Cardinals
+ATL: Atlanta Falcons
+BAL: Baltimore Ravens
+BUF: Buffalo Bills
+CAR: Carolina Panthers
+CHI: Chicago Bears
+CIN: Cincinnati Bengals
+CLE: Cleveland Browns
+DAL: Dallas Cowboys
+DEN: Denver Broncos
+DET: Detroit Lions
+GB: Green Bay Packers
+HOU: Houston Texans
+IND: Indianapolis Colts
+JAX: Jacksonville Jaguars
+KC: Kansas City Chiefs
+MIA: Miami Dolphins
+MIN: Minnesota Vikings
+NE: New England Patriots
+NO: New Orleans Saints
+NYG: New York Giants
+NYJ: New York Jets
+OAK: Oakland Raiders
+PHI: Philadelphia Eagles
+PIT: Pittsburgh Steelers
+SD: San Diego Chargers
+SEA: Seattle Seahawks
+SF: San Francisco 49ers
+STL: Saint Louis Rams
+TB: Tampa Bay Buccaneers
+TEN Tennessee Titans
+WAS: Washington Redskins
+*/
